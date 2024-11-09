ca2350c8fff9cd7b417e76a61df59ead21c7ef8c
AleksandrMaslov <amaslov8910@gmail.com>
2024-11-09 16:10:10 +0300

    fix: db init

--- postgres/init/initpg.sh
+++ postgres/init/initpg.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS "$POSTGRES_DB";
+    CREATE DATABASE "$POSTGRES_DB";
 
     USE "$POSTGRES_DB";
     CREATE TABLE mytable (i INT);
