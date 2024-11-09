9eefd030109aefd2675b24f1e1a2308e1bc4edb0
AleksandrMaslov <amaslov8910@gmail.com>
2024-11-09 10:33:01 +0300

    add: api test path

--- service/index.js
+++ service/index.js
@@ -17,26 +17,7 @@ const run = async () => {
   await client.query('CREATE TABLE IF NOT EXISTS mytable (i integer);')
   console.log('Table created');
 
-
-  app.get('/set-increment', async (req, res) => {
-    console.log('Set request');
-
-    try {
-      const last = await client.query('select i from mytable order by i desc limit 1')
-      let toInsert = 0
-      if (last.rows.length === 0) {
-        toInsert = 1
-      } else {
-        toInsert = last.rows[0].i + 1
-      }
-      await client.query('insert into mytable (i) values ($1)', [toInsert])
-      res.send('increment updated')
-    } catch (error) {
-      res.send(`${error.message}`)
-    }
-  })
-
-  app.get('/api/set-increment', async (req, res) => {
+  app.get('/api/increment/set-increment', async (req, res) => {
     console.log('Set request /api');
 
     try {
@@ -54,36 +35,7 @@ const run = async () => {
     }
   })
 
-  app.get('api/set-increment', async (req, res) => {
-    console.log('Set request api');
-
-    try {
-      const last = await client.query('select i from mytable order by i desc limit 1')
-      let toInsert = 0
-      if (last.rows.length === 0) {
-        toInsert = 1
-      } else {
-        toInsert = last.rows[0].i + 1
-      }
-      await client.query('insert into mytable (i) values ($1)', [toInsert])
-      res.send('increment updated')
-    } catch (error) {
-      res.send(`${error.message}`)
-    }
-  })
-
-  app.get('/get-increment', async (req, res) => {
-    console.log('Get request');
-
-    try {
-      const result = await client.query('select max(i) from mytable')
-      res.send({ max: result.rows[0].max })
-    } catch (error) {
-      res.send(`${error.message}`)
-    }
-  })
-
-  app.get('/api/get-increment', async (req, res) => {
+  app.get('/api/increment/get-increment', async (req, res) => {
     console.log('Get request /api');
 
     try {
@@ -93,18 +45,6 @@ const run = async () => {
       res.send(`${error.message}`)
     }
   })
-
-  app.get('api/get-increment', async (req, res) => {
-    console.log('Get request api');
-
-    try {
-      const result = await client.query('select max(i) from mytable')
-      res.send({ max: result.rows[0].max })
-    } catch (error) {
-      res.send(`${error.message}`)
-    }
-  })
-
 
   app.listen(PORT, () => { console.log(`App listening at port: ${PORT}`) })
 }
