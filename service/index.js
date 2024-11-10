38846c7e74867ebbe7e630d43a70ff508c503667
AleksandrMaslov <amaslov8910@gmail.com>
2024-11-10 14:31:33 +0300

    fix: nginx and service

--- service/index.js
+++ service/index.js
@@ -4,49 +4,17 @@ const cors = require('cors');
 const client = require('./client')
 
 const PORT = process.env.SERVICE_PORT || 5000
-const API_PATH = 'SERVICE_API_PATH'
 
 const app = express()
 app.use(cors());
 app.options('*', cors());
 
 const run = async () => {
-  console.log(`API_PATH: ${API_PATH}`);
-
   await client.connect()
   console.log('DB client connected');
 
   await client.query('CREATE TABLE IF NOT EXISTS mytable (i integer);')
   console.log('Table created');
-
-  app.get(`${API_PATH}/set-increment`, async (req, res) => {
-    console.log('Set request /api/increment');
-
-    try {
-      const last = await client.query('select i from mytable order by i desc limit 1')
-      let toInsert = 0
-      if (last.rows.length === 0) {
-        toInsert = 1
-      } else {
-        toInsert = last.rows[0].i + 1
-      }
-      await client.query('insert into mytable (i) values ($1)', [toInsert])
-      res.send('increment updated')
-    } catch (error) {
-      res.send(`${error.message}`)
-    }
-  })
-
-  app.get(`${API_PATH}/get-increment`, async (req, res) => {
-    console.log('Get request /api/increment');
-
-    try {
-      const result = await client.query('select max(i) from mytable')
-      res.send({ max: result.rows[0].max })
-    } catch (error) {
-      res.send(`${error.message}`)
-    }
-  })
 
   app.get(`/set-increment`, async (req, res) => {
     console.log('Set request simple');
